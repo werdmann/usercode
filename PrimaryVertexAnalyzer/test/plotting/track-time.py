7494c7faf55e46a01e4725deea8148abc085c0c5
Wolfram Erdmann <wolfram.erdmann@psi.ch>
2020-06-19 18:49:56 +0200

    add more plots

--- PrimaryVertexAnalyzer/test/plotting/track-time.py
+++ PrimaryVertexAnalyzer/test/plotting/track-time.py
@@ -38,6 +38,18 @@ F.drawLegend()
 F.Print("track-time-pulls.png")
 
 
+c2a = canvas("landscape")
+F = F2a = Frame("normalized time residuals (#sigma_{t}<0.1)", "track (trec - tsim) / error",-10., 10.)
+for f in tf:
+    h = f.Get("tpulltrk_sigmat01_selmatched")
+    h.Scale(1./h.Integral())
+    #h.Fit("gaus", "", "same")
+    F.add(h, f.marker)
+    keep(h)
+F.drawLegend()
+F.Print("track-time-pulls-small-sigmat.png")
+
+
 c3 = canvas("landscape")
 F = F3 = Frame("normalized time residuals for pions", "track (trec - tsim) / error",-10., 10.)
 for f in tf:
@@ -48,6 +60,19 @@ for f in tf:
     keep(h)
 F.drawLegend()
 F.Print("pion-track-time-pulls.png")
+
+
+c3a = canvas("landscape")
+F = F3a = Frame("normalized time residuals for pions (#sigma_{t}<0.1)", "track (trec - tsim) / error",-10., 10.)
+for f in tf:
+    h = f.Get("tpulltrk_sigmat01_selpion")
+    h.Scale(1./h.Integral())
+    #h.Fit("gaus", "", "same")
+    F.add(h, f.marker)
+    keep(h)
+F.drawLegend()
+F.Print("pion-track-time-pulls-small-sigmat.png")
+
 
 c4 = canvas("landscape")
 F = F4 = Frame("normalized time residuals for kaons", "track (trec - tsim) / error",-10., 10.)
@@ -60,8 +85,19 @@ for f in tf:
 F.drawLegend()
 F.Print("kaon-track-time-pulls.png")
 
+c4a = canvas("landscape")
+F = F4a = Frame("normalized time residuals for kaons (#sigma_{t}<0.1)", "track (trec - tsim) / error",-10., 10.)
+for f in tf:
+    h = f.Get("tpulltrk_sigmat01_selkaon")
+    h.Scale(1./h.Integral())
+    #h.Fit("gaus", "", "same")
+    F.add(h, f.marker)
+    keep(h)
+F.drawLegend()
+F.Print("kaon-track-time-pulls-small-sigmat.png")
+
 c5 = canvas("landscape")
-F = F4 = Frame("track time errors", "track error (ns)",0., 0.1)
+F = F4 = Frame("track time errors", "track error (ns)",0., 0.1, logy=True)
 for f in tf:
     h = f.Get("terrtrk_rec_selmatched")
     h.Scale(1./h.Integral())
